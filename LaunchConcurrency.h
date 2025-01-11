c9a63fdf41387f61b1183759e0d05a7ec25d0018
Full Stack Development Template <Jasper.Assembly@icloud.com>
2025-01-11 19:15:04 +1000

    Add files via upload
    TestBench

---
+++ LaunchConcurrency.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "LaunchConcurrency_Control.h"
+#include "Global_ConcurrentQue.h"
+
+namespace ConcurrentQue
+{
+    class LaunchConcurrency
+    {
+    public:
+        LaunchConcurrency();
+        virtual ~LaunchConcurrency();
+
+        void Concurrent_Thread_Start(
+            class LaunchConcurrency_Control* ptr_LaunchConcurrency_Control,
+            unsigned char concurrent_CoreId,
+            class Global_ConcurrentQue* ptr_Global
+        );
+        void Initialise_Control(
+            class Global_ConcurrentQue* ptr_Global,
+            unsigned char ptr_MyNumImplementedCores
+        );
+        void Thread_End(
+            class LaunchConcurrency_Control* ptr_LaunchConcurrency_Control,
+            unsigned char concurrent_CoreId,
+            class Global_ConcurrentQue* ptr_Global
+        );
+        class LaunchConcurrency_Control* Get_Control_Of_LaunchConcurrency();
+        class Global_ConcurrentQue* Get_GlobalForLaunchConcurrency();
+
+    protected:
+
+    private:
+        static class Global_ConcurrentQue* ptr_Global;
+        static class LaunchConcurrency_Control* ptr_LaunchConcurrency_Control;
+    };
+}
